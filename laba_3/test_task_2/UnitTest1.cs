f6e5b091acd2e70d57e05196868aee737a96ac49
Даниил Кочергов <15350066@study.bsuir.by>
2022-03-24 12:42:04 +0300

    Bug fix

--- laba_3/test_task_2/UnitTest1.cs
+++ laba_3/test_task_2/UnitTest1.cs
@@ -7,33 +7,32 @@ namespace test_task_2
 	public class UnitTest1
 	{
 		[Theory]
-		[InlineData(0, 0, 0, 2, 0)]
-		[InlineData(1, 1, 0, 1, 1)]
+		[InlineData(1, 1, 0, 1, 0.770151152934069)]
 		public void Y_Test__Min(int a, int b, int z, int expectedBranch, double expected)
 		{
 			int actualBranch;
-			Assert.Equal(expected, Task2.X(a, b, z, out actualBranch), 8);
+			Assert.Equal(expected, Task2.Y(a, b, z, out actualBranch), 8);
 			Assert.Equal(expectedBranch, actualBranch);
 		}
 
 		[Theory]
-		[InlineData(100, 100, 1e4-1, 1, 10000)]
-		[InlineData(100, 10-1, 1e3-1, 2, 899100.0007001715)]
+		[InlineData(100, 100, 1e4-1, 1, 93.11594361438)]
+		[InlineData(100, 10-1, 1e3-1, 2, 107.55495817997)]
 		public void Y_Test__Max(int a, int b, int z, int expectedBranch, double expected)
 		{
 			int actualBranch;
-			Assert.Equal(expected, Task2.X(a, b, z, out actualBranch), 8);
+			Assert.Equal(expected, Task2.Y(a, b, z, out actualBranch), 8);
 			Assert.Equal(expectedBranch, actualBranch);
 		}
 
 
 		[Theory]
-		[InlineData(8, 5, 1e3-1, 2, 39960.00070017143)]
-		[InlineData(8, 50, 1e2-1, 1, 497.55803681580704)]
+		[InlineData(8, 5, 1e3-1, 2, 9.977753104514)]
+		[InlineData(8, 50, 1e2-1, 1, -21.919953428988)]
 		public void Y_Test__Mid(int a, int b, int z, int expectedBranch, double expected)
 		{
 			int actualBranch;
-			Assert.Equal(expected, Task2.X(a, b, z, out actualBranch), 8);
+			Assert.Equal(expected, Task2.Y(a, b, z, out actualBranch), 8);
 			Assert.Equal(expectedBranch, actualBranch);
 		}
 
