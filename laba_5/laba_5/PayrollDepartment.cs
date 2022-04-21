74e647c3cd7330671d51481d12c467217b93b39c
Даниил Кочергов <15350066@study.bsuir.by>
2022-04-21 11:16:35 +0300

    Added laba 5

---
+++ laba_5/laba_5/PayrollDepartment.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_5
+{
+	class PayrollDepartment
+	{
+		private List<Employee> LstEmployees { get; set; } = new List<Employee>();
+		private List<Work> LstWorks { get; set; } = new List<Work>();
+
+		public void AddEmployee(string surname)
+		{
+			LstEmployees.Add(new Employee(surname));
+		}
+
+		public void AddWork(string workName, long salary, Work.WorkType type)
+		{
+			LstWorks.Add(new Work(workName, salary, type));
+		}
+
+		public void AddWorkForEmployee(string employeeSurname, string workName)
+		{
+			Work work = new Work();
+			foreach (var curWork in LstWorks)
+			{
+				if (curWork.Name == workName)
+				{
+					work = curWork;
+					break;
+				}
+			}
+
+			Employee employee = new Employee();
+			foreach (var curEmployee in LstEmployees)
+			{
+				if (curEmployee.Surname == employeeSurname)
+				{
+					employee = curEmployee;
+					break;
+				}
+			}
+
+			employee.LstWorksOfEmployee.Add(work);
+		}
+
+		public long GetSalaryBySurname(string surname)
+		{
+			Employee employee = new Employee();
+			foreach (var curEmployee in LstEmployees)
+			{
+				if (curEmployee.Surname == surname)
+				{
+					employee = curEmployee;
+					break;
+				}
+			}
+
+			return employee.GetSalary();
+		}
+
+		public long GetTotalPayment()
+		{
+			long total = 0;
+			foreach (var curEmployee in LstEmployees)
+			{
+				total += curEmployee.GetSalary();
+			}
+
+			return total;
+		}
+	}
+}
---
+++ laba_5/laba_5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace laba_5
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			PayrollDepartment department = new PayrollDepartment();
+			department.AddEmployee("Surname1");
+			department.AddEmployee("Surname2");
+
+			department.AddWork("Work1", 10, Work.WorkType.OutsideOffice);
+			department.AddWork("Work2", 200, Work.WorkType.OutsideOffice);
+			department.AddWork("Work3", 3000, Work.WorkType.InOffice);
+
+			department.AddWorkForEmployee("Surname1", "Work1");
+			department.AddWorkForEmployee("Surname1", "Work2");
+			department.AddWorkForEmployee("Surname2", "Work2");
+			department.AddWorkForEmployee("Surname2", "Work3");
+
+			Console.WriteLine(department.GetSalaryBySurname("Surname1"));
+			Console.WriteLine(department.GetSalaryBySurname("Surname2"));
+			Console.WriteLine(department.GetTotalPayment());
+		}
+	}
+}
